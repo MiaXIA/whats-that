6172e4d3080b1858730325589a569467f3d8aa93
McX <summer.mc.xia@gmail.com>
2017-11-17 19:39:37 -0500

    UI design

---
+++ Whats_that_McX/view controllers/FavoriteListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteListTableViewCell.swift
+//  Whats_that_McX
+//
+//  Created by 夏蓦辰 on 2017/11/17.
+//  Copyright © 2017年 GW Mobile 17. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Whats_that_McX/view controllers/TwitSearchTimelineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TwitSearchTimelineViewController.swift
+//  Whats_that_McX
+//
+//  Created by 夏蓦辰 on 2017/11/17.
+//  Copyright © 2017年 GW Mobile 17. All rights reserved.
+//
+
+import UIKit
+
+class TwitSearchTimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
