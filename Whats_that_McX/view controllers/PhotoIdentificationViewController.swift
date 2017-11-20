0088a452fe4a06a88eabd950b205aa0069fe39a9
McX <summer.mc.xia@gmail.com>
2017-11-19 19:44:12 -0500

    Added privacy, first look on UIImagePickerController

--- Whats_that_McX/view controllers/PhotoIdentificationViewController.swift
+++ Whats_that_McX/view controllers/PhotoIdentificationViewController.swift
@@ -8,28 +8,56 @@
 
 import UIKit
 
-class PhotoIdentificationViewController: UIViewController {
-
+class PhotoIdentificationViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var identifyPhoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func liberary(_ sender: Any) {
+        //check the info if allow the album privacy
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            //initial image picker
+            let picker = UIImagePickerController()
+            //set the delegate
+            picker.delegate = self
+            //set allow editing
+            picker.allowsEditing = true
+            //set the type
+            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            //show the view
+            self.present(picker, animated: true, completion: {
+                () -> Void in
+            })
+        }else{
+            print("error when reading the album")
+        }
+    }
+    
+    //After choose a photo
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //check the info object
+        print(info)
+        
+        //show the image
+        let image:UIImage!
+        //get the original image
+        image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        identifyPhoto.image = image
+        //图片控制器退出
+        picker.dismiss(animated: true, completion: {
+            () -> Void in
+        })
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
